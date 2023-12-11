35caaff06a886497adc75beb0bf3ecd9618267b0
sk sohel rahaman <sohelrahaman43@gmail.com>
2023-12-11 12:56:41 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./pages/Login";
+import CreateBlog from "./pages/CreateBlog";
+import MyBlogs from "./pages/MyBlogs";
+import Homepage from "./pages/Homepage";
+import Header from "./components/common/Header";
+import Users from "./pages/Users";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/create-blog" element={<CreateBlog />}></Route>
+        <Route path="/my-blogs" element={<MyBlogs />}></Route>
+        <Route path="/homepage" element={<Homepage />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import BlogCard from "../components/Blogs/BlogCard";
+
+function Homepage() {
+  const [homepageBlogs, setHomepageBlogs] = useState();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (!token) {
+      window.location.href = "/login";
+    } else {
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/blog/homepage-blogs`, {
+          headers: {
+            "X-Acciojob": token,
+          },
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            setHomepageBlogs(res.data.data);
+          } else {
+            alert(res.data.message);
+          }
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  }, [token]);
+
+  return (
+    <div>
+      <h1 style={{ margin: "30px" }}>Homepage</h1>
+      {homepageBlogs?.map((blog) => (
+        <BlogCard blogData={blog} homepage={true} />
+      ))}
+    </div>
+  );
+}
+
+export default Homepage;
